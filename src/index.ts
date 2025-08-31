64460b8bac533cf9da3cb3b7a2c34e3cda37c002
chaim_cymerman <chaimcymerman0548492309@gmail.com>
2025-08-31 20:22:56 +0300

    add HLD

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-﻿// src/server.ts
-import express from "express";
-// import { Allocator,  } from "./allocator";
-// import { VehicleKind, Car, Floor, SpotSize } from "./types/types";
+﻿import express from "express";
+
 import router from "./router";
 
 const app = express();
@@ -9,23 +7,6 @@ const port = 3000;
 
 app.use(express.json());
 
-// const floors = [
-//   {
-//     id: "floor1",
-//     spots: [
-//       { id: "f1s1", size: SpotSize.MOTORCYCLE },
-//       { id: "f1s2", size: SpotSize.COMPACT },
-//       { id: "f1s3", size: SpotSize.LARGE },
-//     ],
-//   },
-//   {
-//     id: "floor2",
-//     spots: [
-//       { id: "f2s1", size: SpotSize.COMPACT },
-//       { id: "f2s2", size: SpotSize.LARGE },
-//     ],
-//   },
-// ];
 
 app.use("/", router);
 
@@ -33,25 +14,3 @@ app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
-// why to use a database of MongoDB and not a SQL database?
-
-// folre :[
-//   id :
-//   name 
-// ]
-
-// spot :[
-//   floreID
-//   id 
-//   size 
-//   occupieByCarID
-// ]
-
-
-// car : [
-//   id 
-//   kind
-
-// ]
-
-// SELECT * FROM spot WHRE occupieByCarID IS NULL AND size = 'LARGE' LIMIT 1;
